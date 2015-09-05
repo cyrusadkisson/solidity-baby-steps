077b8ae561afc31762e9543ac5e70c79c332d592
Cyrus <c@mailcyr.us>
2015-09-05 08:48:44 -0400

    Edited ping/pong according to forum feedback. Still doesn't work.

--- contracts/45_ping.sol
+++ contracts/45_ping.sol
@@ -1,58 +1,64 @@
 // Supposedly, contracts can get non-constant return values from other contracts. (Whereas you can't from web3/geth.)
 // These two contracts are meant to test this. Like so:
 
-// 1. Deploy Pong.       (SEE PONG CONTRACT)
+// 1. Deploy Pong with a pongval.
 // 2. Deploy Ping, giving it the address of Pong.
-// 3. Set a variable in Pong. 			   (SEE PONG CONTRACT)			
-// 4a/b. Retrieve the variable in Pong *through Ping*, using a sendTransaction from geth -> Ping -> Pong.       (SEE PONG CONTRACT)
+// 3. Call Ping.getPongvalRemote() using a sendTransaction...
+// 4. ... which retreives the value of pongval from Pong.
 // 5. If successful Ping.getPongval() should return the value from step 3.
 
-contract Ping {
+contract PongvalRetriever {
+	int8 public pongval;
+}
 
+contract Ping is PongvalRetriever {
+
+	PongvalRetriever pvr;
     address creator;
-    address pongAddress;
-    int8 pongval;
+    int8 public pongval = -1;
 
 	/*********
  	 Step 2: Deploy Ping, giving it the address of Pong.
  	 *********/
-    function Ping(address _pongAddress) 
+    function Ping(PongvalRetriever _pongAddress) 
     {
-        creator = msg.sender; 								
-        pongval = -1;
-        pongAddress = _pongAddress;
+        creator = msg.sender; 	
+        pvr = _pongAddress;
     }
 
 	/*********
-     Step 4a: Transactionally retrieve pongval from Pong. 
+     Step 3: Transactionally retrieve pongval from Pong. 
      *********/
 
 	function getPongvalRemote() 
 	{
-		pongval = pongAddress.call("getPongval");
+		pongval = pvr.pongval();
 	}
 	
 	/*********
      Step 5: Get pongval (which was previously retrieved from Pong via transaction)
      *********/
      
-    function getPongval() constant returns (uint8)
+    function getPongval() constant returns (int8)
     {
     	return pongval;
     }
 	
-    /*********
-     Functions to get and set pongAddress in case the constructor doesn't work.
+  
+// -----------------------------------------------------------------------------------------------------------------	
+	
+	/*********
+     Functions to get and set pongAddress just in case
      *********/
     
-    function setPongAddress(address _pongAddress)
+    function setPongAddress(PongvalRetriever _pongAddress)
 	{
-		pongAddress = _pongAddress;
+		pvr = _pongAddress;
 	}
 	
-	function getPongAddress() constant return (address)
+	function getPongAddress() constant returns (address)
 	{
-		return pongAddress;
+		return pvr;
 	}
     
     /*********
@@ -67,3 +73,79 @@ contract Ping {
         }
     }
 }
+
+
+/*
+
+var _pongAddress = 0x73a1f27d7179ff28251dd87373b61218c7e3cb0a;
+
+var pingContract = web3.eth.contract([{
+    "constant": false,
+    "inputs": [{
+        "name": "_pongAddress",
+        "type": "address"
+    }],
+    "name": "setPongAddress",
+    "outputs": [],
+    "type": "function"
+}, {
+    "constant": false,
+    "inputs": [],
+    "name": "getPongvalRemote",
+    "outputs": [],
+    "type": "function"
+}, {
+    "constant": false,
+    "inputs": [],
+    "name": "kill",
+    "outputs": [],
+    "type": "function"
+}, {
+    "constant": true,
+    "inputs": [],
+    "name": "pongval",
+    "outputs": [{
+        "name": "",
+        "type": "int8"
+    }],
+    "type": "function"
+}, {
+    "constant": true,
+    "inputs": [],
+    "name": "getPongval",
+    "outputs": [{
+        "name": "",
+        "type": "int8"
+    }],
+    "type": "function"
+}, {
+    "constant": true,
+    "inputs": [],
+    "name": "getPongAddress",
+    "outputs": [{
+        "name": "",
+        "type": "address"
+    }],
+    "type": "function"
+}, {
+    "inputs": [{
+        "name": "_pongAddress",
+        "type": "address"
+    }],
+    "type": "constructor"
+}]);
+
+var ping = pingContract.new(
+    _pongAddress, {
+        from: web3.eth.accounts[0],
+        data: '60606040527fffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff600160146101000a81548160ff02191690837f01000000000000000000000000000000000000000000000000000000000000009081020402179055506040516020806104478339016040526060805190602001505b33600160006101000a81548173ffffffffffffffffffffffffffffffffffffffff021916908302179055507fffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff600160146101000a81548160ff02191690837f010000000000000000000000000000000000000000000000000000000000000090810204021790555080600060016101000a81548173ffffffffffffffffffffffffffffffffffffffff021916908302179055505b506103098061013e6000396000f30060606040523615610074576000357c01000000000000000000000000000000000000000000000000000000009004806339df1608146100765780633af948171461008957806341c0e1b514610096578063809e9924146100a35780638bc889e7146100c7578063fab43cb1146100eb57610074565b005b610087600480359060200150610204565b005b610094600450610122565b005b6100a1600450610262565b005b6100ae6004506102f6565b604051808260000b815260200191505060405180910390f35b6100d26004506101e8565b604051808260000b815260200191505060405180910390f35b6100f6600450610233565b604051808273ffffffffffffffffffffffffffffffffffffffff16815260200191505060405180910390f35b600060019054906101000a900473ffffffffffffffffffffffffffffffffffffffff1673ffffffffffffffffffffffffffffffffffffffff1663809e9924604051817c01000000000000000000000000000000000000000000000000000000000281526004018090506020604051808303816000876161da5a03f1156100025750505060405151600160146101000a81548160ff02191690837f01000000000000000000000000000000000000000000000000000000000000009081020402179055505b565b6000600160149054906101000a900460000b9050610201565b90565b80600060016101000a81548173ffffffffffffffffffffffffffffffffffffffff021916908302179055505b50565b6000600060019054906101000a900473ffffffffffffffffffffffffffffffffffffffff16905061025f565b90565b600160009054906101000a900473ffffffffffffffffffffffffffffffffffffffff1673ffffffffffffffffffffffffffffffffffffffff163373ffffffffffffffffffffffffffffffffffffffff1614156102f357600160009054906101000a900473ffffffffffffffffffffffffffffffffffffffff1673ffffffffffffffffffffffffffffffffffffffff16ff5b5b565b600160149054906101000a900460000b8156',
+        gas: 1000000
+    },
+    function(e, contract) {
+        if (typeof contract.address != 'undefined') {
+            console.log(e, contract);
+            console.log('Contract mined! address: ' + contract.address + ' transactionHash: ' + contract.transactionHash);
+        }
+    })
+    
+    */
--- contracts/45_pong.sol
+++ contracts/45_pong.sol
@@ -1,46 +1,51 @@
 // Supposedly, contracts can get non-constant return values from other contracts. (Whereas you can't from web3/geth.)
 // These two contracts are meant to test this. Like so:
 
-// 1. Deploy Pong.
+// 1. Deploy Pong with a pongval.
 // 2. Deploy Ping, giving it the address of Pong.
-// 3. Set a variable in Pong.
-// 4. Retrieve the variable in Pong *through Ping*, using a sendTransaction from geth -> Ping -> Pong.
+// 3. Call Ping.getPongvalRemote() using a sendTransaction...
+// 4. ... which retreives the value of pongval from Pong.
 // 5. If successful Ping.getPongval() should return the value from step 3.
 
+contract PongvalRetriever {
+	int8 public pongval;
+}
 
-contract Pong {
+contract Pong is PongvalRetriever{
 
     address creator;
-	int8 pongval;
 
 	/*********
  	 Step 1: Deploy Pong
  	 *********/
-    function Pong() 
+    function Pong(int8 _pongval) 
     {
         creator = msg.sender; 
+        pongval = _pongval;
     }
 	
 	/*********
-	 Step 3. Set pongval.
+	 Step 4. Transactionally return pongval.
 	 *********/	
+//	function getPongvalTransactional() public returns (int8)
+//    {
+//    	return pongval;
+//    }
+    
+// ----------------------------------------------------------------------------------------------------------------------------------------
+    
+    /*********
+	 pongval getter/setter, just in case.
+	 *********/
+	function getPongvalConstant() public constant returns (int8)
+    {
+    	return pongval;
+    } 
+	 	
 	function setPongval(int8 _pongval)
 	{
 		pongval = _pongval;
 	}
-	
-	/*********
-	 Step 4b. Transactionally return pongval.
-	 *********/	
-	function getPongval() returns (int8)
-    {
-    	return pongval;
-    }
-    
-    function getPongvalConstant() constant returns (int8)
-    {
-    	return pongval;
-    }
 	
     /**********
      Standard kill() function to recover funds 
@@ -57,6 +62,8 @@ contract Pong {
 
 /*
 
+var _pongval = -56;
+
 var pongContract = web3.eth.contract([{
     "constant": false,
     "inputs": [{
@@ -67,36 +74,46 @@ var pongContract = web3.eth.contract([{
     "outputs": [],
     "type": "function"
 }, {
-    "constant": false,
+    "constant": true,
     "inputs": [],
-    "name": "kill",
-    "outputs": [],
-    "type": "function"
-}, {
-    "constant": false,
-    "inputs": [],
-    "name": "getPongval",
+    "name": "getPongvalConstant",
     "outputs": [{
         "name": "",
         "type": "int8"
     }],
     "type": "function"
 }, {
+    "constant": false,
     "inputs": [],
+    "name": "kill",
+    "outputs": [],
+    "type": "function"
+}, {
+    "constant": true,
+    "inputs": [],
+    "name": "pongval",
+    "outputs": [{
+        "name": "",
+        "type": "int8"
+    }],
+    "type": "function"
+}, {
+    "inputs": [{
+        "name": "_pongval",
+        "type": "int8"
+    }],
     "type": "constructor"
 }]);
-
-var pong = pongContract.new({
-    from: web3.eth.accounts[0],
-    data: '60606040525b33600060006101000a81548173ffffffffffffffffffffffffffffffffffffffff021916908302179055505b610184806100406000396000f30060606040526000357c01000000000000000000000000000000000000000000000000000000009004806323a1c2711461004f57806341c0e1b5146100625780638bc889e71461006f5761004d565b005b610060600480359060200150610093565b005b61006d6004506100f0565b005b61007a6004506100d4565b604051808260000b815260200191505060405180910390f35b80600060146101000a81548160ff02191690837f01000000000000000000000000000000000000000000000000000000000000009081020402179055505b50565b6000600060149054906101000a900460000b90506100ed565b90565b600060009054906101000a900473ffffffffffffffffffffffffffffffffffffffff1673ffffffffffffffffffffffffffffffffffffffff163373ffffffffffffffffffffffffffffffffffffffff16141561018157600060009054906101000a900473ffffffffffffffffffffffffffffffffffffffff1673ffffffffffffffffffffffffffffffffffffffff16ff5b5b56',
-    gas: 1000000
-}, function(e, contract) {
-    if (typeof contract.address != 'undefined') {
-        console.log(e, contract);
-        console.log('Contract mined! address: ' + contract.address + ' transactionHash: ' + contract.transactionHash);
-    }
-});
-
-0x83e61a143f7b9e281948ce53f4c46c7124ec9dab
-
+var pong = pongContract.new(
+    _pongval, {
+        from: web3.eth.accounts[0],
+        data: '606060405260405160208061025c8339016040526060805190602001505b33600060016101000a81548173ffffffffffffffffffffffffffffffffffffffff0219169083021790555080600060156101000a81548160ff02191690837f01000000000000000000000000000000000000000000000000000000000000009081020402179055505b506101c6806100966000396000f30060606040526000357c01000000000000000000000000000000000000000000000000000000009004806323a1c2711461005a57806340193d171461006d57806341c0e1b514610091578063809e99241461009e57610058565b005b61006b6004803590602001506100f1565b005b6100786004506100d5565b604051808260000b815260200191505060405180910390f35b61009c600450610132565b005b6100a96004506100c2565b604051808260000b815260200191505060405180910390f35b600060009054906101000a900460000b81565b6000600060159054906101000a900460000b90506100ee565b90565b80600060156101000a81548160ff02191690837f01000000000000000000000000000000000000000000000000000000000000009081020402179055505b50565b600060019054906101000a900473ffffffffffffffffffffffffffffffffffffffff1673ffffffffffffffffffffffffffffffffffffffff163373ffffffffffffffffffffffffffffffffffffffff1614156101c357600060019054906101000a900473ffffffffffffffffffffffffffffffffffffffff1673ffffffffffffffffffffffffffffffffffffffff16ff5b5b56',
+        gas: 1000000
+    },
+    function(e, contract) {
+        if (typeof contract.address != 'undefined') {
+            console.log(e, contract);
+            console.log('Contract mined! address: ' + contract.address + ' transactionHash: ' + contract.transactionHash);
+        }
+    })
 */
