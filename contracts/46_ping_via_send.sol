753337f9cfcc2490bf139333bb9ad5e5e7f013a4
Cyrus <c@mailcyr.us>
2015-09-22 14:38:39 -0400

    nc

--- contracts/46_ping_via_send.sol
+++ contracts/46_ping_via_send.sol
@@ -1,3 +1,14 @@
+/***
+ *     _    _  ___  ______ _   _ _____ _   _ _____ 
+ *    | |  | |/ _ \ | ___ \ \ | |_   _| \ | |  __ \
+ *    | |  | / /_\ \| |_/ /  \| | | | |  \| | |  \/
+ *    | |/\| |  _  ||    /| . ` | | | | . ` | | __ 
+ *    \  /\  / | | || |\ \| |\  |_| |_| |\  | |_\ \
+ *     \/  \/\_| |_/\_| \_\_| \_/\___/\_| \_/\____/
+ *                                                 
+ *   This contract DOES NOT WORK at the moment. 9/22/2015                                     
+ */
+
 // 1. Deploy Pong .
 // 2. Deploy Ping, giving it the address of Pong.
 // 3. Call Ping.touchPong() using a <pongaddress>.send()
