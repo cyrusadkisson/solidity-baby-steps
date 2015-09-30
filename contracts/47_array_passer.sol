ee56a8bd59e695a7a6ded93236be8a0e763f676f
Cyrus <c@mailcyr.us>
2015-09-30 11:51:56 -0400

    Reworked arraypasser, although it's buggy.

--- contracts/47_array_passer.sol
+++ contracts/47_array_passer.sol
@@ -6,15 +6,16 @@
  *    \  /\  / | | || |\ \| |\  |_| |_| |\  | |_\ \
  *     \/  \/\_| |_/\_| \_\_| \_/\___/\_| \_/\____/
  *                                                 
- *   This contract DOES NOT WORK due to a solidity bug.
- *   It should compile but does not. 9/20/2015                                     
+ *   This contract DOES NOT WORK                                   
  */
 
 contract Descriptor {
     
-	function getDescription() constant returns (uint16[5]){	
-		uint16[5] somevar;
-		//somevar = 456; 
+	function getDescription() constant returns (uint16[3]){	
+		uint16[3] somevar;
+		somevar[0] = 34;
+		somevar[1] = 76;
+		somevar[2] = 48;
 		return somevar;
 	}
 }
@@ -24,10 +25,10 @@ contract ArrayPasser {
     address creator;
     
     /***
-     * 1. Declare a 9x9 map of Tiles
+     * 1. Declare a 3x3 map of Tiles
      ***/
-    uint8 mapsize = 9;
-    Tile[9][9] tiles; 
+    uint8 mapsize = 3;
+    Tile[3][3] tiles; 
 
     struct Tile 
     {
@@ -44,25 +45,56 @@ contract ArrayPasser {
      * 3. Upon construction, initialize the internal map elevations.
      *      The Descriptors start uninitialized.
      ***/
-    function ArrayPasser(uint8[] incmap) 
+    function ArrayPasser(uint8[9] incmap) 
     {
         creator = msg.sender;
-        uint counter = 0;
-        Descriptor nothing;
+        uint8 counter = 0;
         for(uint8 y = 0; y < mapsize; y++)
        	{
            	for(uint8 x = 0; x < mapsize; x++)
            	{
-           	    tiles[x][y].descriptor = nothing;
            		tiles[x][y].elevation = incmap[counter]; 
+           		counter = counter + 1;
            	}	
         }	
     }
+   
+    /***
+     * 4. After contract mined, check the map elevations
+     ***/
+    function getElevations() constant returns (uint8[3][3])
+    {
+        uint8[3][3] memory elevations;
+        for(uint8 y = 0; y < mapsize; y++)
+        {
+        	for(uint8 x = 0; x < mapsize; x++)
+        	{
+        		elevations[x][y] = tiles[x][y].elevation; 
+        	}	
+        }	
+    	return elevations;
+    }
+   
+   /***
+     * 5. Load descriptors
+     ***/
+     
+    function loadDescriptors() 
+    {
+        Descriptor d = new Descriptor();
+        for(uint8 y = 0; y < mapsize; y++)
+       	{
+           	for(uint8 x = 0; x < mapsize; x++)
+           	{
+           	    tiles[x][y].descriptor = d;
+           	}	
+        }	
+    } 
     
    /*** 
-    * 4. get Description of a tile at x,y
+    * 6. get Description of a tile at x,y
     ***/ 
-    uint16[5] anothervar;
+    uint16[3] anothervar;
     function getTileDescription(uint8 x, uint8 y)
     {
     	Descriptor desc = tiles[x][y].descriptor;       // get the descriptor for this tile
@@ -73,6 +105,15 @@ contract ArrayPasser {
     	// save it to a variable for constant retrieval elsewhere
     	
     	return; 
+    }
+    
+    /*** 
+    * 7. retrieve it
+    ***/ 
+    
+    function retrieveAnothervar() constant returns (uint16[3])
+    {
+        return anothervar;
     }
     
     /**********
