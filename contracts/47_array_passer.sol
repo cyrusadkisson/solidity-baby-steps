da456c561dfb2494a5da4fd0c69c24d684352727
Cyrus <c@mailcyr.us>
2015-09-30 16:07:39 -0400

    Simplified ArrayPasser to show geth bug.

--- contracts/47_array_passer.sol
+++ contracts/47_array_passer.sol
@@ -1,26 +1,14 @@
-/***
- *     _    _  ___  ______ _   _ _____ _   _ _____ 
- *    | |  | |/ _ \ | ___ \ \ | |_   _| \ | |  __ \
- *    | |  | / /_\ \| |_/ /  \| | | | |  \| | |  \/
- *    | |/\| |  _  ||    /| . ` | | | | . ` | | __ 
- *    \  /\  / | | || |\ \| |\  |_| |_| |\  | |_\ \
- *     \/  \/\_| |_/\_| \_\_| \_/\___/\_| \_/\____/
- *                                                 
- *   This contract DOES NOT WORK                                   
- */
+import "mortal";
 
-contract Descriptor {
+// contract Descriptor {
     
-	function getDescription() constant returns (uint16[3]){	
-		uint16[3] somevar;
-		somevar[0] = 34;
-		somevar[1] = 76;
-		somevar[2] = 48;
-		return somevar;
-	}
-}
+// 	function getDescription() constant returns (uint16[3]){	
+// 		uint16[3] somevar;
+// 		return somevar;
+// 	}
+// }
 
-contract ArrayPasser {
+contract ArrayPasser is mortal {
 
     address creator;
     
@@ -36,9 +24,8 @@ contract ArrayPasser {
          * 2. A tile is comprised of the owner, elevation and a pointer to a 
          *      contract that explains what the tile looks like
          ****/
-        address owner;
         uint8 elevation;
-        Descriptor descriptor;
+        //Descriptor descriptor;
     }
     
     /***
@@ -73,57 +60,5 @@ contract ArrayPasser {
         	}	
         }	
     	return elevations;
-    }
-   
-   /***
-     * 5. Load descriptors
-     ***/
-     
-    function loadDescriptors() 
-    {
-        Descriptor d = new Descriptor();
-        for(uint8 y = 0; y < mapsize; y++)
-       	{
-           	for(uint8 x = 0; x < mapsize; x++)
-           	{
-           	    tiles[x][y].descriptor = d;
-           	}	
-        }	
-    } 
-    
-   /*** 
-    * 6. get Description of a tile at x,y
-    ***/ 
-    uint16[3] anothervar;
-    function getTileDescription(uint8 x, uint8 y)
-    {
-    	Descriptor desc = tiles[x][y].descriptor;       // get the descriptor for this tile
-    	anothervar = desc.getDescription();  // get the description from the descriptor
-    	
-    	// TODO validate the description
-    	// TODO convert it to JSON
-    	// save it to a variable for constant retrieval elsewhere
-    	
-    	return; 
-    }
-    
-    /*** 
-    * 7. retrieve it
-    ***/ 
-    
-    function retrieveAnothervar() constant returns (uint16[3])
-    {
-        return anothervar;
-    }
-    
-    /**********
-     Standard kill() function to recover funds 
-     **********/
-    function kill()
-    { 
-        if (msg.sender == creator)
-        {
-            suicide(creator);  // kills this contract and sends remaining funds back to creator
-        }
     }
 }
