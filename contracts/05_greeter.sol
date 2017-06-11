3232a74b5bb2cb6cc7b00ba1fa591e5b44c2b27e
David Kullmann <kullmann.david@gmail.com>
2017-06-11 11:46:19 -0400

    05_greeter.sol correct "creator" typo

--- contracts/05_greeter.sol
+++ contracts/05_greeter.sol
@@ -5,7 +5,7 @@
 
 contract Greeter         // The contract definition. A constructor of the same name will be automatically called on contract creation. 
 {
-    address creator;     // At first, an empty "address"-type variable of the name "owner". Will be set in the constructor.
+    address creator;     // At first, an empty "address"-type variable of the name "creator". Will be set in the constructor.
     string greeting;     // At first, an empty "string"-type variable of the name "greeting". Will be set in constructor and can be changed.
 
     function Greeter(string _greeting) public   // The constructor. It accepts a string input and saves it to the contract's "greeting" variable.
