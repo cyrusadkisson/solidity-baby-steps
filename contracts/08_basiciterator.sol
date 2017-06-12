334a36b97c1f1b2851a8ea0147f341444f0c51d9
Cyrus <fivedogit@users.noreply.github.com>
2017-06-12 05:10:06 -0400

    Create 08_basiciterator.sol

--- contracts/08_basiciterator.sol
+++ contracts/08_basiciterator.sol
@@ -4,21 +4,21 @@
 
 contract BasicIterator {
 
-    address creator;
-    uint8[10] integers;
+    address creator;                  // reserve one "address"-type spot
+    uint8[10] integers;               // reserve a chunk of storage for 10 8-bit unsigned integers in an array
 
     function BasicIterator() 
     {
-        creator = msg.sender;
-        uint8 x = 0;
-        while(x < integers.length)
+        creator = msg.sender;         // set the creator address
+        uint8 x = 0;                  // initialize an 8-bit, unsigned integer to zero
+        while(x < integers.length)    // the variable integers was initialized to length 10
         {
-        	integers[x] = x;
+        	integers[x] = x;      // set integers to [0,1,2,3,4,5,6,7,8,9] over ten iterations
         	x++;
         }
     }
     
-    function getSum() constant returns (uint)
+    function getSum() constant returns (uint)    
     {
     	uint8 sum = 0;
     	uint8 x = 0;
